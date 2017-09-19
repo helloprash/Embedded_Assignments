99d101066ab2d6c1f6f9d83d2c34a9a9beb88da2
prashrox7 <31364644+prashrox7@users.noreply.github.com>
2017-09-19 22:18:49 +0530

    Update StringLength.c.c

--- 17-Sept-2017/StringLength.c.c
+++ 17-Sept-2017/StringLength.c.c
@@ -1,14 +1,22 @@
 //To find the length of the string
 #include<stdio.h>
-main()
+
+int mystrlen(char name[])
 {
-	char name[100];			//Array to store the string
 	int n=0;
-	printf("Enter the string : ");
-	scanf("%s",&name);
 	while(name[n]!='\0')
 	{	
-		n++;
+	     n++;
 	}
-	printf("\nLength of the string=%d\n",n);
+	return n;
+}
+
+main()
+{
+	char name[100];		//Array to store the string
+	int length;
+	printf("Enter the string : ");
+	scanf("%s",&name);
+	length=mystrlen(name);
+	printf("\nLength of the string=%d\n",length);
 }
