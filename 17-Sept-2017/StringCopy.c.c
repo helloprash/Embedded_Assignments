51806f40104c9caf11f2f568ce77074277e61e3e
prashrox7 <31364644+prashrox7@users.noreply.github.com>
2017-09-19 22:08:44 +0530

    Update StringCopy.c.c

--- 17-Sept-2017/StringCopy.c.c
+++ 17-Sept-2017/StringCopy.c.c
@@ -1,17 +1,23 @@
 //Copying the string from source to destination
 #include<stdio.h>
-main()
+char src[100];			//Source string
+char dest[100];			//Destination string
+
+char mystrcpy(char src[],char dest[])
 {
-	char src[100];			//Source string
-	char dest[100];			//Destination string
 	int n=0;
-	printf("source: ");
-	scanf("%s",&src);
 	dest[0]=src[0];
 	while(dest[n]!='\0')
 	{	
 		n++;
 		dest[n]=src[n];
 	}
-	printf("Destination:%s",dest);
+}
+
+main()
+{
+	printf("Source: ");
+	scanf("%s",&src);
+	mystrcpy(src,dest);
+	printf("Destination: %s",dest);
 }
